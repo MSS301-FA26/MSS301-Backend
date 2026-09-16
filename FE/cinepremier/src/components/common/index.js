09b709c746f1e94e6a2d230a298ef009ad0baa82
HieuTuan <tuannhse182788@fpt.edu.vn>
2026-09-16 20:59:24 +0700

    chore: remove FE folder from backend repository

--- FE/cinepremier/src/components/common/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Button } from './Button';
-export { default as EmptyState } from './EmptyState';
-export { default as LoadingState } from './LoadingState';
-export { default as Modal } from './Modal';
-export { default as MovieCard } from './MovieCard';
-export { default as Table } from './Table';
-export { default as Toast } from './Toast';
--- FE/cinepremier/src/utils/helpers.js
+++
@@ -1,8 +0,0 @@
-export const formatCurrency = (value) => `${Number(value || 0).toLocaleString('vi-VN')}đ`;
-
-export const formatDate = (value, options = {}) => {
-  if (!value) return '';
-  const date = new Date(value);
-  if (Number.isNaN(date.getTime())) return '';
-  return date.toLocaleDateString('vi-VN', options);
-};
